de8716de0c04d154049fae45a01ebde84e960336
l.ligg <35821916+lauranliggan@users.noreply.github.com>
2019-12-13 10:42:30 -0800

    Add files via upload
    added figures for Paeopigments

--- CHLa/CHLa_2018.R
+++ CHLa/CHLa_2018.R
@@ -19,6 +19,8 @@
 #   installed, remove the comment and run if necessary, otherwise the majority
 #   of summaries and figures will not work.
 
+
+
 ## Install packages ----
 install.packages(c("plyr", "dplyr", "ggplot2", "magrittr", "lubridate", "knitr", "reshape2", "gridExtra", "googlesheets", "plotly", "tidyr"))
 
@@ -124,7 +126,7 @@ LL2019_CHLaData$year<- year(LL2019_CHLaData$date)
 glimpse(LL2019_CHLaData)
 
 # filter out data above 1m
-LL2019_CHLaData1m<- filter(LL2019_CHLaData, line_out_depth < 1)
+LL2019_CHLaData1m<- filter(LL2019_CHLaData, line_out_depth < 1, Phaeo < 10)
 
 # --- Clean data end ---
 
@@ -158,6 +160,8 @@ pd <- 0.5
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#660066", "#666600")
 
+
+#CHLa
 # by site
 # trends between sites
 
@@ -188,12 +192,13 @@ CHLaFig_print <- ggsave(plot = CHLaFigSites, width = 15, height = 17, dpi = 600,
 
 
 #dot plot of all sites
-CHLaPlot <- ggplot(data=CHlaSum2019, aes(x=month, 
+CHLaPlot <- ggplot(data=LL2019_CHLaData1m, aes(x=month, 
                                      y=Chla, col=Site)) +
-  geom_point(shape = 16, size = 3, position=position_dodge(pd)) +
+  geom_point(position=position_dodge(pd),size=3) +
+
   scale_fill_manual(values = cbPalette) +
   scale_color_manual(values = cbPalette) +
-  facet_wrap(~year) +
+  facet_grid(year~plankton) +
   ggtitle(~italic("Nearshore Surface Water"), "CHLa") +
   ylab("Chlorophyll (microg/L)") +
   xlab("Month") +
@@ -206,7 +211,7 @@ CHLaPlot <- ggplot(data=CHlaSum2019, aes(x=month,
 CHLaPlot
 
 # Print 
-CHla2018Fig_print <- ggsave(plot =  CHLaPlot, width = 12, height = 8, dpi = 600, filename = "CHLaPlot_2019_all sites.png")
+CHla2018Fig_print <- ggsave(plot =  CHLaPlot, width = 12, height = 15, dpi = 600, filename = "CHLaPlot_2019_all sites.png")
 
 # trends accross all sites
 
@@ -277,6 +282,169 @@ grid.draw(g)
 # Print 
 CHla2019Fig_print <- ggsave(plot = g, width = 7, height = 10, dpi = 600, filename = "CHLa_2019_all sites.png")
 
+
+CHLa2019Fig <- ggplot(LL2019_CHLaData1m, aes(x= month, y= Chla)) +
+  geom_boxplot(aes(), size=0.8) +
+  geom_jitter(aes(color = Site), size=2, alpha = 0.5, position=position_dodge(pd)) +
+  scale_color_manual(values = cbPalette) + 
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  ylab("Chlorophyll (microg/L)") +
+  xlab("Month")+ 
+  facet_grid(year~plankton) +
+  theme(axis.text.x=element_text(hjust=1)) +
+  ggtitle(~italic("Nearshore Surface Water"), "CHLa (<1m depth)") + 
+  theme(plot.title = element_text(hjust = -0.01, vjust=1.12, size =20)) +
+  theme(legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0.01,1), legend.text = element_text(size = 12)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))  
+
+
+CHLa2019Fig
+
+# Print 
+CHla2018Fig_print <- ggsave(plot =  CHLa2019Fig, width = 12, height = 13, dpi = 600, filename = "CHLaPlot_2019.png")
+
+
+#PHAEO
+# by site
+# trends between sites
+
+PhaeoFigSites <- ggplot(data=PhaeoSum2019, aes(x=month, 
+                                             y=Phaeo, 
+                                             col= plankton,
+                                             ymin=Phaeo-se,
+                                             ymax=Phaeo+se)) +
+  geom_errorbar( position=position_dodge(pd), width=.5) +
+  ylab("Phaeo (microg/L)") +
+  xlab("Month")+ 
+  geom_point( position=position_dodge(pd), size=3) +
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  geom_line( position=position_dodge(pd), aes(group=plankton, linetype= plankton)) +
+  facet_grid(Site~year)+
+  theme(axis.text=element_text(size=12))+
+  scale_color_manual(values = cbPalette) +
+  theme(axis.text.x=element_text(hjust=1)) +
+  theme(legend.justification=c(0,1), legend.position=c(0.01,0.99), legend.text = element_text(size = 12)) +
+  ggtitle(~italic("Nearshore Surface Water"), "Phaeopigment") +
+  theme(axis.text.x=element_text(hjust=1, angle=45))  
+
+PhaeoFigSites
+
+# Print DO13C
+PhaeoFig_print <- ggsave(plot = PhaeoFigSites, width = 15, height = 17, dpi = 600, filename = "Phaeo_2019_by site.png")
+
+
+
+#dot plot of all sites
+PhaeoPlot <- ggplot(data=LL2019_CHLaData1m, aes(x=month, 
+                                               y=Phaeo, col=Site)) +
+  geom_point(position=position_dodge(pd),size=3) +
+  
+  scale_fill_manual(values = cbPalette) +
+  scale_color_manual(values = cbPalette) +
+  facet_grid(year~plankton) +
+  ggtitle(~italic("Nearshore Surface Water"), "Phaeopigment") +
+  ylab("Phaeo (microg/L)") +
+  xlab("Month")+ 
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  theme(axis.text.x=element_text(hjust=1)) +
+  theme(legend.justification=c(0,1), legend.position=c(0.01,0.97), legend.text = element_text(size = 12), legend.background = element_rect(linetype = 2, size = 0.5, colour = 1)) +
+  theme(plot.title = element_text(hjust = -0.01, vjust=1.12, size =20)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+PhaeoPlot
+
+# Print 
+PhaeoPlotFig_print <- ggsave(plot =  PhaeoPlot, width = 12, height = 15, dpi = 600, filename = "PhaeoPlot_2019_all sites.png")
+
+# trends accross all sites
+
+
+Phaeo2017Fig <- ggplot(data=subset(PhaeoSum2019, year == "2017"), aes(month, Phaeo, col=plankton)) +
+  geom_boxplot() + 
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  scale_color_manual(values = cbPalette) + 
+  ylab("Phaeo (microg/L)") +
+  xlab("Month")+ 
+  theme(axis.text.x=element_text(hjust=1)) +
+  theme(axis.text.y=element_text(hjust=1)) +
+  ggtitle("Phaeopigments 2017 (<1m depth)") + 
+  theme(plot.title = element_text(hjust = -0.01, vjust=1.12, size =20)) +
+  theme(legend.justification=c(0,1), legend.position=c(0.8,0.97),legend.text = element_text(size = 12)) +
+  theme(axis.text=element_text(size=12))+
+  theme(axis.title.x=element_blank(),axis.text.x=element_blank())  
+
+
+Phaeo2017Fig
+
+
+Phaeo2018Fig <- ggplot(data=subset(PhaeoSum2019, year == "2018"), aes(month, Phaeo, col=plankton)) +
+  geom_boxplot() + 
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  scale_color_manual(values = cbPalette) + 
+  ylab("Phaeo (microg/L)") +
+  xlab("Month")+ 
+  theme(axis.text.x=element_text(hjust=1)) +
+  theme(axis.text.y=element_text(hjust=1)) +
+  ggtitle("Phaeopigments 2018") + 
+  theme(plot.title = element_text(hjust = -0.01, vjust=1.12, size =20)) +
+  theme(legend.position="none") +
+  theme(axis.text=element_text(size=12))+
+  theme(axis.title.x=element_blank(),axis.text.x=element_blank())  
+
+
+Phaeo2018Fig
+
+
+Phaeo2019Fig <- ggplot(data=subset(PhaeoSum2019, year == "2019"), aes(month, Phaeo, col=plankton)) +
+  geom_boxplot() + 
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  scale_color_manual(values = cbPalette) + 
+  ylab("Phaeo (microg/L)") +
+  xlab("Month")+ 
+  theme(axis.text.x=element_text(hjust=1)) +
+  theme(axis.text.y=element_text(hjust=1)) +
+  ggtitle("Phaeopigments 2019") + 
+  theme(plot.title = element_text(hjust = -0.01, vjust=1.12, size =20)) +
+  theme(legend.position="none") +
+  theme(axis.text=element_text(size=12))+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+
+Phaeo2019Fig
+
+# stack all graphs into one figure
+
+library(grid)
+gl = lapply(list(Phaeo2017Fig, Phaeo2018Fig,Phaeo2019Fig), ggplotGrob) 
+library(gtable)
+g = do.call(rbind, c(gl, size="first"))
+g$widths = do.call(unit.pmax, lapply(gl, "[[", "widths"))
+grid.newpage()
+grid.draw(g) 
+
+# Print 
+Phaeo2019Fig_print <- ggsave(plot = g, width = 7, height = 10, dpi = 600, filename = "Phaeo_2019_all sites.png")
+
+
+Phaeo2019Fig <- ggplot(LL2019_CHLaData1m, aes(x= month, y= Phaeo)) +
+  geom_boxplot(aes(), size=0.8) +
+  geom_jitter(aes(color = Site), size=2, alpha = 0.5, position=position_dodge(pd)) +
+  scale_color_manual(values = cbPalette) + 
+  theme(panel.grid.major = element_line(colour = "grey90", linetype = 3)) +
+  ylab("Phaeo (microg/L)") +
+  xlab("Month")+ 
+  facet_grid(year~plankton) +
+  theme(axis.text.x=element_text(hjust=1)) +
+  ggtitle(~italic("Nearshore Surface Water"), "Phaeopigments (<1m depth)") + 
+  theme(plot.title = element_text(hjust = -0.01, vjust=1.12, size =20)) +
+  theme(legend.title=element_blank(), legend.justification=c(0,1), legend.position=c(0.01,1), legend.text = element_text(size = 12)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))  
+
+
+Phaeo2019Fig
+
+# Print 
+Phaeo2019Fig_print <- ggsave(plot =  Phaeo2019Fig, width = 12, height = 13, dpi = 600, filename = "PhaeoPlot_2019.png")
 
 
 # --- Summary figures end ---
